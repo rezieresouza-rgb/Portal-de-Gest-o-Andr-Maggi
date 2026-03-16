467340f735a42f694ab5994c79fce02338732822
Reziere Souza <rezie@example.com>
2026-03-16 00:57:09 -0400

    feat: atualizar cardápio de quinta-feira (5ª semana)

--- constants/menus.ts
+++ constants/menus.ts
@@ -203,7 +203,13 @@ export const OFFICIAL_MENUS: WeeklyMenu[] = [
         entradaIngredients: ["MAÇÃ"],
         ingredients: ["ALHO", "ARROZ BRANCO", "CARNE BOVINA CUBOS", "CEBOLA", "CEBOLINHA", "COLORAU", "FEIJÃO CARIOCA", "ÓLEO DE SOJA", "PIMENTA DE CHEIRO", "REPOLHO", "RÚCULA", "SAL", "SALSA"] 
       },
-      { day: "Quinta", dish: "SALADA DE ALFACE COM CENOURA, ESTROGONOFE DE CARNE SUÍNA E ARROZ + PÃO COM REQUEIJÃO E LEITE COM CACAU", ingredients: ["ALFACE", "CENOURA", "CARNE SUÍNA", "ARROZ BRANCO", "REQUEIJÃO CREMOSO", "LEITE", "CACAU EM PÓ"] },
+      { 
+        day: "Quinta", 
+        dish: "SALADA DE ALFACE COM CENOURA, ESTROGONOFE DE CARNE SUÍNA E ARROZ", 
+        entradaDish: "PÃO COM REQUEIJÃO E LEITE COM CACAU",
+        entradaIngredients: ["AÇÚCAR CRISTAL", "CACAU EM PÓ", "LEITE", "PÃO", "REQUEIJÃO CREMOSO"],
+        ingredients: ["ALFACE", "ALHO", "ARROZ BRANCO", "CARNE SUÍNA", "CEBOLA", "CEBOLINHA", "CENOURA", "EXTRATO DE TOMATE", "FARINHA DE TRIGO", "LEITE", "LIMÃO", "MANTEIGA", "ÓLEO DE SOJA", "SAL", "SALSA"] 
+      },
       { day: "Sexta", dish: "ARROZ CUIABANO E FEIJÃO + PONCÃ", ingredients: ["CARNE BOVINA CUBOS", "BANANA DA TERRA", "ARROZ BRANCO", "FEIJÃO CARIOCA", "PONCÃ"] }
     ]
   }
