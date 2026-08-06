0d3a89ca94ec57e4031df7388c08fa8696e197e5
Reziere Souza <rezie@example.com>
2026-08-06 17:25:56 -0400

    feat: adicionar rendimento geral oficial do 7º ano no 2º bimestre

--- components/CoordinationExternalGrades.tsx
+++ components/CoordinationExternalGrades.tsx
@@ -1155,6 +1155,13 @@ const CoordinationExternalGrades: React.FC<CoordinationExternalGradesProps> = ({
                                              else if (subjUpper === 'CIÊNCIAS') b2Val = 41;
                                              else if (subjUpper === 'GEOGRAFIA') b2Val = 41;
                                              else if (subjUpper === 'HISTÓRIA') b2Val = 35;
+                                          } else if (grade === '7º ANO') {
+                                             if (subjUpper === 'GEOGRAFIA') b2Val = 46;
+                                             else if (subjUpper === 'LÍNGUA PORTUGUESA') b2Val = 44;
+                                             else if (subjUpper === 'HISTÓRIA') b2Val = 37;
+                                             else if (subjUpper === 'ARTE') b2Val = 36;
+                                             else if (subjUpper === 'MATEMÁTICA') b2Val = 32;
+                                             else if (subjUpper === 'CIÊNCIAS') b2Val = 31;
                                           } else if (b2Ass.length > 0) {
                                              b2Val = b2Ass.reduce((acc, curr) => acc + curr.averageScore, 0) / b2Ass.length;
                                           }
